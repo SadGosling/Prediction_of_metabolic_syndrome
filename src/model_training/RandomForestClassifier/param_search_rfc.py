53cf68cfd0d4a598b1951a6980ac655c966acb09
Tishchenko Alexey <164168101+SadGosling@users.noreply.github.com>
2024-11-24 21:32:19 +0300

    Create param_search_rfc.py

---
+++ src/model_training/RandomForestClassifier/param_search_rfc.py
@@ -0,0 +1,13 @@
+def param_search_rfc(trial):
+    params = {
+        "criterion": 'log_loss',
+        "max_depth": trial.suggest_int("max_depth", 3, 10),
+        "n_estimators": trial.suggest_int("n_estimators", 100, 1000, 100),
+        "min_samples_split": trial.suggest_int("min_samples_split", 2, 10, 2),
+        "min_samples_leaf": trial.suggest_int("min_samples_leaf", 1, 4, 1)
+    }
+
+    model = RandomForestClassifier(**params)
+    model.fit(x_train_imp, y_train)
+    predictions = model.predict(x_test_imp)
+    return f1_score(y_test, predictions)
