25d12f4f025f07949898506bb223a22818fb8140
Tishchenko Alexey <164168101+SadGosling@users.noreply.github.com>
2024-11-24 21:40:20 +0300

    Create get_best_cb.py

---
+++ src/model_training/CatBoostClassifier/get_best_cb.py
@@ -0,0 +1,11 @@
+def get_best_cb(x_train, y_train):
+    study_cb = optuna.create_study(direction='maximize')
+    study_cb.optimize(param_search_cb, n_trials=30)
+    cb_best = CatBoostClassifier(learning_rate=study_cb.best_params['learning_rate'],
+                             depth=study_cb.best_params['depth'],
+                             random_seed=42,
+                             subsample=study_cb.best_params['subsample'],
+                             colsample_bylevel=study_cb.best_params['colsample_bylevel'],
+                             min_data_in_leaf =study_cb.best_params['min_data_in_leaf'],
+                             random_strength=study_cb.best_params['random_strength'])
+    return cb_best
