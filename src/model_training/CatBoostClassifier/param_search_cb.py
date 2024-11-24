5b3c9ad018041ae07707217f1fc36157e9adaf8f
Tishchenko Alexey <164168101+SadGosling@users.noreply.github.com>
2024-11-24 21:39:54 +0300

    Create param_search_cb.py

---
+++ src/model_training/CatBoostClassifier/param_search_cb.py
@@ -0,0 +1,15 @@
+def param_search_cb(trial):
+    params = {
+        "iterations": 1000,
+        "learning_rate": trial.suggest_float("learning_rate", 1e-3, 0.1, log=True),
+        "depth": trial.suggest_int("depth", 1, 10),
+        "subsample": trial.suggest_float("subsample", 0.05, 1.0),
+        "colsample_bylevel": trial.suggest_float("colsample_bylevel", 0.05, 1.0),
+        "min_data_in_leaf": trial.suggest_int("min_data_in_leaf", 1, 100),
+        "random_strength": trial.suggest_float("random_strength", 0.5, 2.)
+    }
+
+    model = CatBoostClassifier(**params, silent=True)
+    model.fit(x_train_selected, y_train)
+    predictions = model.predict(x_test_selected)
+    return f1_score(y_test, predictions)
