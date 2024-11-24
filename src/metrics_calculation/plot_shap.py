f76295c4314300431b85aab05d3a6ae1b4a241c7
Tishchenko Alexey <164168101+SadGosling@users.noreply.github.com>
2024-11-25 01:41:40 +0300

    Update plot_shap.py

--- src/metrics_calculation/plot_shap.py
+++ src/metrics_calculation/plot_shap.py
@@ -1,4 +1,5 @@
 def plot_shap(model, x_train):
+    """Функция, рисующая средние значения SHAP для переданной модели на датасете"""
     explainer = shap.TreeExplainer(model)
     shap_values = explainer(x_train)
     shap.plots.bar(shap_values)
