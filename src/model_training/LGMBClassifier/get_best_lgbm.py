08345bf6ad0abc522b73a5925da1655c48c361c4
Tishchenko Alexey <164168101+SadGosling@users.noreply.github.com>
2024-11-24 21:38:44 +0300

    Create get_best_lgbm.py

---
+++ src/model_training/LGMBClassifier/get_best_lgbm.py
@@ -0,0 +1,9 @@
+def get_best_lgbm(x_train, y_train):
+    study_lgbm = optuna.create_study(direction='maximize')
+    study_lgbm.optimize(param_search_lgbm, n_trials=30)
+    lgbm_best = LGBMClassifier(learning_rate=study_lgbm.best_params['learning_rate'],
+                             max_depth=study_lgbm.best_params['max_depth'],
+                             n_estimators = study_lgbm.best_params['n_estimators'],
+                             random_seed=42,
+                             subsample=study_lgbm.best_params['subsample'])
+    return lgbm_best
