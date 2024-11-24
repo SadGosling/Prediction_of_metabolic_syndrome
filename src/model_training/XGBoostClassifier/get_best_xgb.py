0c1b1444e0f225b87b5464c168d9c7f8e93b77d0
Tishchenko Alexey <164168101+SadGosling@users.noreply.github.com>
2024-11-25 01:47:26 +0300

    Update get_best_xgb.py

--- src/model_training/XGBoostClassifier/get_best_xgb.py
+++ src/model_training/XGBoostClassifier/get_best_xgb.py
@@ -1,4 +1,5 @@
 def get_best_xgb(x_train, y_train):
+    """Функция, возвращающая наиболее оптимальную модель XGBClassifier на основе алгоритма Optuna"""
     study_xgb = optuna.create_study(direction='maximize')
     study_xgb.optimize(param_search_xgb, n_trials=30)
     xgb_best = XGBClassifier(learning_rate=study_xgb.best_params['learning_rate'],
