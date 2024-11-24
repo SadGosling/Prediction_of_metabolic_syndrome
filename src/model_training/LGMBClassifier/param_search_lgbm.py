1abf2500303041042a4b9f7297e28f3bf4baa86b
Tishchenko Alexey <164168101+SadGosling@users.noreply.github.com>
2024-11-24 21:38:17 +0300

    Create param_search_lgbm.py

---
+++ src/model_training/LGMBClassifier/param_search_lgbm.py
@@ -0,0 +1,14 @@
+def param_search_lgbm(trial):
+    params = {
+        "learning_rate": trial.suggest_float("learning_rate", 1e-3, 0.1, log=True),
+        "max_depth": trial.suggest_int("max_depth", 100, 500),
+        "n_estimators": trial.suggest_int("n_estimators", 100, 1000, 100),
+        "subsample": trial.suggest_float("subsample", 0.05, 1.0),
+        "colsample_bylevel": trial.suggest_float("colsample_bylevel", 0.05, 1.0),
+        "gamma": trial.suggest_float("gamma", 0.1, 3.)
+    }
+
+    model = LGBMClassifier(**params, silent=True)
+    model.fit(x_train_selected, y_train)
+    predictions = model.predict(x_test_selected)
+    return f1_score(y_test, predictions)
