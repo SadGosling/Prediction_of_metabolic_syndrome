5e9128f29ebd98548ef3b21be52e5c910800b6db
Tishchenko Alexey <164168101+SadGosling@users.noreply.github.com>
2024-11-24 21:36:30 +0300

    Create get_best_xgb.py

---
+++ src/model_training/XGBoostClassifier/get_best_xgb.py
@@ -0,0 +1,11 @@
+def get_best_xgb(x_train, y_train):
+    study_xgb = optuna.create_study(direction='maximize')
+    study_xgb.optimize(param_search_xgb, n_trials=30)
+    xgb_best = XGBClassifier(learning_rate=study_xgb.best_params['learning_rate'],
+                         max_depth=study_xgb.best_params['max_depth'],
+                         n_estimators = study_xgb.best_params['n_estimators'],
+                         random_seed=42,
+                         subsample=study_xgb.best_params['subsample'],
+                         colsample_bylevel= study_xgb.best_params['colsample_bylevel'],
+                         gamma=study_xgb.best_params['gamma'])
+    return xgb_best
