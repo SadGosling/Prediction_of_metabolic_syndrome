36beb82dfc48dbfd8bf8ddcbd5e7f6ad27db1d03
Tishchenko Alexey <164168101+SadGosling@users.noreply.github.com>
2024-11-24 21:44:45 +0300

    Create get_test_scores.py

---
+++ src/metrics_calculation/get_test_scores.py
@@ -0,0 +1,12 @@
+def get_test_scores(model, x_test, y_test):
+  y_pred = model.predict(x_test)
+  round_num = 2
+  result_dict = {
+      "Accuracy": round(accuracy_score(y_test, y_pred), round_num),
+      "AUC": round(roc_auc_score(y_test, y_pred), round_num),
+      "Precision": round(precision_score(y_test, y_pred), round_num),
+      "Recall": round(recall_score(y_test, y_pred), round_num),
+      "F1-score": round(f1_score(y_test, y_pred), round_num),
+      "F2-score": round(f2_score(y_test, y_pred), round_num)
+  }
+  return result_dict
